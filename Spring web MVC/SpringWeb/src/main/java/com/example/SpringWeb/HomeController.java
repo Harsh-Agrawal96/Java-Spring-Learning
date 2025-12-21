91923330acffb63c92c993e4dbcbdfea125c19f1
Harsh-Agrawal96 <harshagrawal9650@gmail.com>
2025-12-21 23:46:43 +0530

    add remain config

--- Spring web MVC/SpringWeb/src/main/java/com/example/SpringWeb/HomeController.java
+++ Spring web MVC/SpringWeb/src/main/java/com/example/SpringWeb/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home(){
         System.out.println("calling home controller");
         return "index";
